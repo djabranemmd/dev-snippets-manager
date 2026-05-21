eb72d8dae798d6c435c031e056a4238051ed1d75
djabranemmd <mammadi.a.djabrane@gmail.com>
2026-05-21 16:52:43 +0100

    I have a big problem i will solve it next time

---
+++ src/components/EmptyState.jsx
@@ -0,0 +1,7 @@
+export default function EmptyState() {
+  return (
+    <div className="mt-10 p-10 text-center border border-dashed rounded-2xl text-slate-500 dark:border-slate-700">
+      No Snippets Found 🚀
+    </div>
+  );
+}
